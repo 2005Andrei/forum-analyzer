83a8d717318ab6d5afa9a4843a12df9b2468aa71
2005Andrei <ascurtuny@gmail.com>
2026-04-25 17:51:35 +0300

    frontend: theme provider

--- frontend/src/components/main/Home.tsx
+++ frontend/src/components/main/Home.tsx
@@ -1,9 +1,11 @@
 import FinalNavbar from "./utils/FinalNavbar";
+import HomeContent from "./utils/HomeContent";
 
 export default function Home() {
     return (
         <>
             <FinalNavbar />
+            <HomeContent />
         </>
     );
 }
